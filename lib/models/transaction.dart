0fa66f2301e7420b2111ab631b7754f9f7ae3d71
Mateus Machado Costa <75508372+MateusMaccos@users.noreply.github.com>
2022-11-08 21:29:19 -0300

    Model com DocumentSnapshot

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Transaction {
   late final String id;
   late final String title;
@@ -9,4 +11,19 @@ class Transaction {
       required this.title,
       required this.value,
       required this.date});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'value': value,
+      'date': date,
+    };
+  }
+
+  Transaction.porDocumentSnapshot(DocumentSnapshot<Map<String, dynamic>> doc) {
+    id = doc.id;
+    title = doc.data()!['title'];
+    value = doc.data()!['value'];
+    date = doc.data()!['date'];
+  }
 }
