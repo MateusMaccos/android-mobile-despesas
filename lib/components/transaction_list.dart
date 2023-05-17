02e14e2c7c1a24fc08ec2f7e9e7eaa33b8370494
Mateus Machado Costa <mateusdoug@hotmail.com>
2023-05-17 09:06:24 -0300

    Atualizado código

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -1,6 +1,5 @@
 import 'package:despesas/components/transaction_item.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import '../models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
